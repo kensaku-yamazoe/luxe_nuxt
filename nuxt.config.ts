a9970dd7ce6dca6c43029c68b49143d5eec6615c
kensaku33 <picow.zoe33@gmail.com>
2023-12-12 14:51:46 +0900

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default {
+  router: {
+    base: "/<repository-name>/",
+  },
   devtools: { enabled: true },
 
   css: ["~/assets/css/tailwind.css"],
